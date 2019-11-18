4020bfaeb78d8368c9c4ccfc43dc8ced7a2f1751
Diego Flores <42399052+Diego1803631@users.noreply.github.com>
2019-11-17 18:29:31 -0600

    res

---
+++ resource.h
@@ -0,0 +1,87 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Avance.rc
+//
+#define IDOK3                           2
+#define ID_FOTO                         3
+#define ID_EDITAR                       3
+#define ID_NUEVA                        4
+#define ID_CANCEL                       4
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     103
+#define IDD_DIALOG5                     104
+#define IDD_DIALOG3                     106
+#define IDR_MENU1                       107
+#define IDD_DIALOG4                     107
+#define IDD_DIALOG6                     108
+#define IDD_DIALOG7                     109
+#define IDR_MENU2                       110
+#define IDD_DIALOG8                     110
+#define IDD_DIALOG9                     111
+#define IDD_DIALOG10                    112
+#define IDD_DIALOG11                    113
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_USUARIO                     1003
+#define IDC_CN                          1003
+#define IDC_CC                          1004
+#define IDC_INFO                        1005
+#define IDC_COMBO2                      1007
+#define IDC_COMBO3                      1008
+#define IDC_CD                          1008
+#define IDC_COMBO4                      1009
+#define IDC_DESCRIPCION                 1009
+#define IDC_CS                          1009
+#define IDC_COMBO5                      1010
+#define IDC_COMBO6                      1011
+#define IDC_COMBO7                      1012
+#define IDC_LIST1                       1015
+#define IDC_LIST2                       1016
+#define IDC_NOMBRE                      1016
+#define IDC_RADIO1                      1017
+#define IDC_RADIO2                      1018
+#define IDC_FECHA                       1019
+#define IDC_MATERIA                     1020
+#define IDC_CLAVE                       1021
+#define IDC_CREDITOS                    1022
+#define IDC_HORAS                       1023
+#define IDC_SEMESTRE                    1024
+#define IDC_APELLIDO                    1025
+#define IDC_CAL                         1026
+#define IDC_CONTRA                      1027
+#define IDC_IMAGE                       1028
+#define IDC_TIPO                        1029
+#define ID_ELIMINAR                     1030
+#define ID_BUSC                         1031
+#define IDC_BA                          1032
+#define ID_BUSC2                        1033
+#define ID_INS                          1034
+#define ID_SALIR_SALIRYGUARDAR          40001
+#define ID_REGISTRO_CORDINADOR          40002
+#define ID_REGISTRO_SEMESTRE            40003
+#define ID_REGISTRO_MATERIAS            40004
+#define ID_VER_CALIFICACIONES           40005
+#define ID_B40006                       40006
+#define ID_NUEVO_ALUMNO                 40007
+#define ID_ALUMNO_INSCRIBIR             40008
+#define ID_ALUMNO_CALIFICACIONES        40009
+#define ID_ARCHIVO_SALIR                40010
+#define ID_VER_KARDEX                   40011
+#define ID_NUEVO_CORDINADOR             40012
+#define ID_NUEVO_COORDINADOR            40013
+#define ID_NUEVO_SEMESTRE               40014
+#define ID_NUEVO_MATERIA                40015
+#define ID_BUSQUEDA                     40016
+#define ID_B40017                       40017
+#define IDC_BUSQUEDA                    40018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_COMMAND_VALUE         40019
+#define _APS_NEXT_CONTROL_VALUE         1035
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
